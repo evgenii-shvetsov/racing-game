8e5f99969ffc0f4cdbce82379ac800f3efa23e01
evgenii-shvetsov <sea.dev91@gmail.com>
2022-12-02 09:51:15 -0800

    complete setup dependencies

---
+++ src/index.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+
+    const canvas = document.getElementById("game-canvas");
+    const ctx = canvas.getContext("2d");
+
+    canvas.width = 500;
+    canvas.height = 500;
+
+
+});
