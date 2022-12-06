1b9615e2b8f1db7cc26721f71f9fdb3b375a3e0a
evgenii-shvetsov <sea.dev91@gmail.com>
2022-12-05 23:47:52 -0800

    change animation

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 // import StopWatch from './scripts/stopwatch';
 import DriveWay from './scripts/driveway';
 import MovingObject from './scripts/moving_objects';
-
 // import DrawingObjects from './scripts/moving_objects'
 
 document.addEventListener("DOMContentLoaded", () => {
 
 console.log("hey, script was loaded")
 
-window.addEventListener("keydown", function (e) { KeyDown(e); }); //change position of moving objects on canvas
+window.addEventListener("keydown", function (e) { KeyDown(e); }); 
 
 
 ///////////STOPWATCH CODE//////////////
@@ -79,20 +78,17 @@ function stopWatch() {
 ///////////STOPWATCH CODE  END//////////////
 
 
-//should be deleted
+
 // let clock;
 // let stopWatch = new StopWatch(clock);
 
-// CANVAS DRAWING OK SO FAR
+// CANVAS DRAWING
 const canvas = document.getElementById("game-canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 626;
-// canvas.height = 800;
 canvas.height = 626;
 
-//launching game screen start
-//OK SO FAR
-
+// WORKING WITH DOM
 let controls = document.querySelector(".controls");
 let play_game = document.getElementById("play_game");
 let pause_game = document.getElementById("pause_game");
@@ -110,16 +106,13 @@ let collisionScreen = document.getElementById('collision-screen');
 let lackOfRangeScreen = document.getElementById('lack-of-rage-screen');
 
 
-
-
-
 play_game.addEventListener("click", (e)=>{
     console.log('play is working');
     LaunchGame();
     // controls.classList.add("hide-controls")   
     play_game.classList.add("hide-controls");
     // pause_game.classList.add("move-pause-button");
-    pause_game.style.display ="block";
+    // pause_game.style.display ="block";
     stopwatch.style.display = "block";
     control_keys.style.display = "block";
     control_keys_space.style.display = "block";
@@ -134,22 +127,19 @@ pause_game.addEventListener("click", (e)=>{
     StopGame()
     // controls.classList.add("hide-controls")   
     play_game.classList.add("hide-controls")
-    
     //stopwatch pause
     clock = false;//clock pause
 })
 
-//launching game screen end
 
 
 //VARIABLES start
 
-// const REFRESH_SCREEN_TIME = 1000 / 50;
 const REFRESH_SCREEN_TIME = 1000 / 50;
 let timerInterval = null;
 
 let size = 0.1 //Moving object size
-let speed = 6;
+let speed = 4;
 let obstacles = []; //store all moving obstacles
 let obstaclesSpeed = 4; //could be triggered in tounds increasing
 
@@ -166,8 +156,10 @@ rageLevel.innerHTML = `Rage ${rage}`
 let disappeardCars = 0
 carsPassed.innerHTML = `Passed cars ${disappeardCars}`
 
+//VARIABLES end
 
-// CAR HORN FUNCTION
+
+// CAR HORN FUNCTION NEED TO REFACTOR AND MOVE TO SEPARATE FILE
 function Sound(src) {
     this.sound = document.createElement("audio");
     this.sound.src = src;
@@ -184,8 +176,8 @@ function Sound(src) {
   }
 let carHorn = new Sound('../assets/car-horn2.wav')
 
-//VARIABLES end
 
+// Function to start/pause game
 function pausingGame(){
     if(timerInterval == null){
         LaunchGame();
@@ -205,62 +197,56 @@ let driveWays = [new DriveWay('../assets/driveway1.jpeg', 0, speed, canvas), new
 let racer = new MovingObject("../assets/car1.png", canvas.width / 2, canvas.height / 1.3, true, speed, size, canvas, obstaclesSpeed); // main racer object
     
     
-// SEPARATE functions START all start with Capital letter
+// MAIN FUNCTIONS
 
-//works fine
 function LaunchGame(){
         // if(!racer.collideWith)
         // {
             timerInterval = setInterval(UpdateGame, REFRESH_SCREEN_TIME); //Refreshing screen n time per second
         // }
-        // requestAnimationFrame(LaunchGame) . check this out
-        // requestAnimationFrame(UpdateGame)
+       
     }
-//works fine
+
 function StopGame(){
         clearInterval(timerInterval); //Stop game
         timerInterval = null;
     }
 
-// adding moving objects
 function UpdateGame(){
         driveWays[0].updateDriveWay(driveWays[1]);
         driveWays[1].updateDriveWay(driveWays[0]);
     
         //Generating obstacles  //best value so far randomNumber(0, 10000) > 9750
 
-        setTimeout(()=>{
+        // setTimeout(()=>{
             if(randomNumber(0, 10000) > 9950){  //car 1
                 obstacles.push(new MovingObject("../assets/cabrio.png", randomNumber(155, canvas.width - 300), randomNumber(150, 350) * -1, false, speed, size, canvas, obstaclesSpeed));
             }
-        },1000)
-
-        setTimeout(()=>{
+        // },1000)
+        
+        // setTimeout(()=>{
             if(randomNumber(0, 10000) > 9950){ //car 2
                 obstacles.push(new MovingObject("../assets/bugatti.png", randomNumber(155, canvas.width - 250), randomNumber(200, 350) * -1, false, speed, size, canvas, obstaclesSpeed));
             }
-        },2000)
+        // },2000)
 
-        setTimeout(()=>{
+        // setTimeout(()=>{
             if(randomNumber(0, 10000) > 9950){ //car 3
                 obstacles.push(new MovingObject("../assets/ferrari_red.png", randomNumber(155, canvas.width - 200), randomNumber(350, 350) * -1, false, speed, size, canvas, obstaclesSpeed));
             }
-        },3000)
+        // },3000)
 
-        setTimeout(()=>{
+        // setTimeout(()=>{
             if(randomNumber(0, 10000) > 9950){ //car 3
                 obstacles.push(new MovingObject("../assets/acura1.png", randomNumber(155, canvas.width - 180), randomNumber(300, 350) * -1, false, speed, size, canvas, obstaclesSpeed));
             }
-        },4000)
+        // },4000)
 
-    
-    
         racer.updateMovingObject();
     
         if(racer.collideWith){
             // alert("!!!!!!!!!OHHHHH CRASH!");
             // StopGame();
-            
         }
     
         let isDestroyed = false; 
@@ -273,12 +259,9 @@ function UpdateGame(){
         }
     
         if(isDestroyed){ //removing cars from canvas and counting them
-
             obstacles.shift();
             disappeardCars++
             carsPassed.innerHTML = `Passed cars ${disappeardCars}`
-            
-            console.log(disappeardCars)
         }
     
         let collision = false;
@@ -286,23 +269,14 @@ function UpdateGame(){
         for(let i = 0; i < obstacles.length; i++){
             collision = racer.hit(obstacles[i]);
     
-            
             if(collision){
                 //adding screen information for a user
                 collisionScreen.style.display = 'block';
                 if(life > 0){
-                    // if(timerInterval == null){
-                    //     LaunchGame();
-                    //     clock = true;
-                    //     stopWatch();
-                    // } else {
-                    //     StopGame();
-                    //     clock = false;
-                    // }
                     pausingGame()
                     life--;
                     let tag = document.createElement("p");
-                    let text = document.createTextNode(`Be carefull,  ${life} collision left`);
+                    let text = document.createTextNode(`Hey, you hit a car! ${life} collisions left`);
                     tag.appendChild(text);
                     collisionScreen.appendChild(tag);
                     setTimeout(()=>{
@@ -311,18 +285,19 @@ function UpdateGame(){
                         LaunchGame();
                         clock = true;
                         stopWatch();
-                    },2000)
-                    // collisionScreen.innerHTML = `BE CAREFULL! ${life} collision left`
-                    
-                    
-                    // alert(`BE CAREFULL! ${life} collision left`)
+                    },2500)
                     obstacles = [];
                     lifeCounter.innerHTML = `Life ${life}`
                 } else{
-                    alert("You're a bad driver")
+                    // alert("You're a bad driver")
                     StopGame();
                     clock = false; // stop clock when collision
                     racer.collideWith = true;
+                    let tag = document.createElement("p");
+                    let text = document.createTextNode("You're a bad driver");
+                    tag.appendChild(text);
+                    collisionScreen.appendChild(tag);
+
                     break;
                 }
                 // StopGame();
@@ -399,7 +374,19 @@ function shakeCanvas(){
             rageLevel.innerHTML = `Rage ${rage}`
             // console.log(rage, "after action")
         } else {
-            alert('No more rage, please chill!')
+            rageLevel.classList.add("rage-level-shake");
+            // alert('No more rage, please chill!')
+            // let tag = document.createElement("p");
+            // let text = document.createTextNode('No more rage, please chill!');
+            // tag.appendChild(text);
+            // collisionScreen.appendChild(tag);
+            // setTimeout(()=>{
+            //     tag.remove();
+            //     collisionScreen.style.display = 'none';
+            //     LaunchGame();
+            //     clock = true;
+            //     stopWatch();
+            // },2500)
         }
 
         setTimeout(()=>{
